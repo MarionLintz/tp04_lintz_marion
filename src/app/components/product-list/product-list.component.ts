04a151f1eb8a625a151e0146b0cee00ffa2c4d0e
Marion Lintz <marionnirina@gmail.com>
2020-10-11 16:41:45 +0200

    implement async observable list

--- src/app/components/product-list/product-list.component.ts
+++ src/app/components/product-list/product-list.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
 import { Product } from 'src/app/interfaces/product';
 import { SelectedFilter } from 'src/app/interfaces/selected-filter';
 import { DataService } from 'src/app/services/data-service/data.service';
@@ -9,36 +11,34 @@ import { DataService } from 'src/app/services/data-service/data.service';
   styleUrls: ['./product-list.component.scss'],
 })
 export class ProductListComponent implements OnInit {
-  public listFilteredProduct: Array<Product>;
-  public listAllProduct: Array<Product>;
+  public listProductObs: Observable<Product[]>;
+  public listFilteredProductObs: Observable<Product[]>;
 
   constructor(private dataService: DataService) {}
 
   ngOnInit(): void {
-    this.dataService.GetList().subscribe((data) => {
-      this.listAllProduct = new Array<Product>();
-      this.listFilteredProduct = new Array<Product>();
-
-      this.listAllProduct.push(...data);
-      this.listFilteredProduct.push(...this.listAllProduct);
-    });
+    this.listProductObs = this.dataService.GetList();
+    this.listFilteredProductObs = this.listProductObs;
   }
 
   onFilterChange(filter: SelectedFilter): void {
-    this.listFilteredProduct = this.listAllProduct.filter((value: Product) => {
-      switch (filter.global) {
-        case 'gender':
-          if(filter.detail !== "") return value.Gender == filter.detail;
-          else return true;
-        case 'name':
-          if (filter.detail !== '') return value.Name.toLowerCase().indexOf(filter.detail.toLowerCase()) > -1;
-          else return true;
-        case 'breed':
-          if (filter.detail !== '') return value.Breed.toLowerCase().indexOf(filter.detail.toLowerCase()) > -1;
-          else return true;
-        default:
-          return true;
-      }
-    });
+    this.listFilteredProductObs = this.listProductObs.pipe(
+      map(data => {
+        return data.filter((value: Product) => {
+          switch (filter.global) {
+            case 'gender':
+              if(filter.detail !== "") return value.Gender == filter.detail;
+              else return true;
+            case 'name':
+              if (filter.detail !== '') return value.Name.toLowerCase().indexOf(filter.detail.toLowerCase()) > -1;
+              else return true;
+            case 'breed':
+              if (filter.detail !== '') return value.Breed.toLowerCase().indexOf(filter.detail.toLowerCase()) > -1;
+              else return true;
+            default:
+              return true;
+          }
+        });
+      }));
   }
 }
